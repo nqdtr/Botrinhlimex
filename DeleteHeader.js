ad65696fd936df7413298d66976a12ae9bbd4b8b
nqdtr <dangtrinh26124@gmail.com>
2025-03-14 23:03:19 +0700

    Update and rename NDT_delete_header.js to DeleteHeader.js

--- DeleteHeader.js
+++ DeleteHeader.js
@@ -1,5 +1,5 @@
+// ===== Botrinhlimex ===== //
 
 const version = 'V2.6.1';
-
 
 function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
