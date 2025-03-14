804a5758fa2b0f790de479135f01f20e60095dc6
nqdtr <dangtrinh26124@gmail.com>
2025-03-14 22:33:51 +0700

    Update NDT_delete_header.js

--- NDT_delete_header.js
+++ NDT_delete_header.js
@@ -1,5 +1,5 @@
 
-const version = 'V1.0.2';
+const version = 'V2.6.1';
 
 
 function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
